7c8af16edae7bf6c0c19293862113bde57572561
Luciano Rivera <lucrivera91@gmail.com>
2024-08-08 22:33:14 -0300

    Ejemplo basico seguridad con Auth0 y react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button, Card, Container, Image, Nav } from "react-bootstrap";
+import loading from "./assets/loading.gif";
 
 function App() {
+  const { isAuthenticated, isLoading, loginWithPopup, logout, user } =
+    useAuth0();
+
+  if (isLoading) {
+    return (
+      <Image
+        src={loading}
+        alt="Loading..."
+        width={100}
+        height={100}
+        className="text-center"
+      />
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav className="bg-secondary pb-2">
+        {!isAuthenticated ? (
+          <Button
+            variant="success"
+            className="ms-auto mt-2 me-2"
+            onClick={() => loginWithPopup()}
+          >
+            Login
+          </Button>
+        ) : (
+          <Button
+            variant="danger"
+            className="ms-auto mt-2 me-2"
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          >
+            Logout
+          </Button>
+        )}
+      </Nav>
+      <Container fluid>
+        {user ? (
+          <Card className="ms-auto me-auto mt-3" style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={user.picture} />
+            <Card.Body>
+              <Card.Title>{user.name}</Card.Title>
+              <Card.Text>{user.email}</Card.Text>
+            </Card.Body>
+          </Card>
+        ) : (
+          <h1>Debe iniciar sesión para ver los datos del usuario.</h1>
+        )}
+      </Container>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-nbp1bzujfqc0mbt0.us.auth0.com"
+      clientId="MJa63O9lzL65600hQN7F0tOHOiY9UTvP"
+      authorizationParams={{ redirect_uri: window.location.origin }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
